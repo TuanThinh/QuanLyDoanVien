7c310873d8de72840fd3f3a17280abfcf1fd6636
tuanthinh <tuanthinhmta@gmail.com>
2018-12-14 00:26:56 +0700

    update entity

--- src/main/java/mta/qldv/entity/HoatDong.java
+++ src/main/java/mta/qldv/entity/HoatDong.java
@@ -17,11 +17,6 @@ public class HoatDong implements Serializable {
 	@Column(name = "ten_hd")
 	private String tenHoatDong;
 
-	@Column(name = "thoi_gian")
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", locale = "vi-VN", timezone = "Asia/Ho_Chi_Minh")
-	@Temporal(TemporalType.DATE)
-	private Date thoiGian;
-
 	@Column(name = "dia_diem")
 	private String diaDiem;
 
@@ -51,14 +46,6 @@ public class HoatDong implements Serializable {
 
 	public void setTenHoatDong(String tenHoatDong) {
 		this.tenHoatDong = tenHoatDong;
-	}
-
-	public Date getThoiGian() {
-		return thoiGian;
-	}
-
-	public void setThoiGian(Date thoiGian) {
-		this.thoiGian = thoiGian;
 	}
 
 	public String getDiaDiem() {
